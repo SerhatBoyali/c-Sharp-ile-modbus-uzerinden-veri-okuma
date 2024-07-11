5e6af07d9a7be5780044fe0a7c47fe3d218f7bf4
Serhat Boyalı <155268386+SerhatBoyali@users.noreply.github.com>
2024-07-11 11:52:26 +0300

    Modbus - ZedGraph grafik ekranı veri aktarımı
    Windows form uygulaması - Modbus protokolü ile seriport dan gelen bilgileri ZedGraph grafik ekranında gösterimi.

---
+++ Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace modbus_projesi_9
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.Button buttonConnect;
+        private System.Windows.Forms.Button buttonDisconnect;
+        private ZedGraph.ZedGraphControl zedGraphControl;
+        private System.Windows.Forms.Timer timer;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.buttonConnect = new System.Windows.Forms.Button();
+            this.buttonDisconnect = new System.Windows.Forms.Button();
+            this.zedGraphControl = new ZedGraph.ZedGraphControl();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // buttonConnect
+            // 
+            this.buttonConnect.Location = new System.Drawing.Point(12, 12);
+            this.buttonConnect.Name = "buttonConnect";
+            this.buttonConnect.Size = new System.Drawing.Size(100, 23);
+            this.buttonConnect.TabIndex = 0;
+            this.buttonConnect.Text = "Bağlan";
+            this.buttonConnect.UseVisualStyleBackColor = true;
+            this.buttonConnect.Click += new System.EventHandler(this.buttonConnect_Click);
+            // 
+            // buttonDisconnect
+            // 
+            this.buttonDisconnect.Location = new System.Drawing.Point(118, 12);
+            this.buttonDisconnect.Name = "buttonDisconnect";
+            this.buttonDisconnect.Size = new System.Drawing.Size(100, 23);
+            this.buttonDisconnect.TabIndex = 1;
+            this.buttonDisconnect.Text = "Bağlantıyı Kes";
+            this.buttonDisconnect.UseVisualStyleBackColor = true;
+            this.buttonDisconnect.Click += new System.EventHandler(this.buttonDisconnect_Click);
+            // 
+            // zedGraphControl
+            // 
+            this.zedGraphControl.Location = new System.Drawing.Point(12, 50);
+            this.zedGraphControl.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.zedGraphControl.Name = "zedGraphControl";
+            this.zedGraphControl.ScrollGrace = 0D;
+            this.zedGraphControl.ScrollMaxX = 0D;
+            this.zedGraphControl.ScrollMaxY = 0D;
+            this.zedGraphControl.ScrollMaxY2 = 0D;
+            this.zedGraphControl.ScrollMinX = 0D;
+            this.zedGraphControl.ScrollMinY = 0D;
+            this.zedGraphControl.ScrollMinY2 = 0D;
+            this.zedGraphControl.Size = new System.Drawing.Size(1692, 853);
+            this.zedGraphControl.TabIndex = 2;
+            this.zedGraphControl.UseExtendedPrintDialog = true;
+            // 
+            // timer
+            // 
+            this.timer.Interval = 1000;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1748, 929);
+            this.Controls.Add(this.zedGraphControl);
+            this.Controls.Add(this.buttonDisconnect);
+            this.Controls.Add(this.buttonConnect);
+            this.Name = "Form1";
+            this.Text = "Modbus Projesi";
+            this.ResumeLayout(false);
+
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Drawing;
+using System.IO.Ports;
+using System.Windows.Forms;
+using Modbus.Device;
+using ZedGraph;
+
+namespace modbus_projesi_9
+{
+    public partial class Form1 : Form
+    {
+        private IModbusSerialMaster master;
+        private SerialPort port;
+        private GraphPane myPane;
+        private PointPairList[] pointPairLists;
+        private LineItem[] curves;
+        private int time;
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeModbus();
+
+            myPane = zedGraphControl.GraphPane;
+            myPane.Title.Text = "Modbus Veri Grafiği";
+            myPane.XAxis.Title.Text = "Zaman";
+            myPane.YAxis.Title.Text = "Değer";
+
+            pointPairLists = new PointPairList[8]; 
+            curves = new LineItem[8];
+            Color[] colors = { Color.Blue, Color.Red, Color.Green, Color.Purple, Color.Orange, Color.Brown, Color.Pink, Color.Gray };
+
+            for (int i = 0; i < 8; i++)
+            {
+                pointPairLists[i] = new PointPairList();
+                curves[i] = myPane.AddCurve($"Veri {i + 1}", pointPairLists[i], colors[i], SymbolType.None);
+            }
+        }
+
+        private void InitializeModbus()
+        {
+            try
+            {
+                port = new SerialPort("COM6"); 
+                port.BaudRate = 9600;
+                port.DataBits = 8;
+                port.Parity = Parity.None;
+                port.StopBits = StopBits.One;
+
+                if (!port.IsOpen)
+                {
+                    port.Open();
+                }
+
+                master = ModbusSerialMaster.CreateRtu(port);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Seri port açılırken hata oluştu: {ex.Message}");
+            }
+        }
+
+        private void buttonConnect_Click(object sender, EventArgs e)
+        {
+            if (port != null && port.IsOpen)
+            {
+                timer.Start();
+            }
+            else
+            {
+                MessageBox.Show("Seri port açık değil!");
+            }
+        }
+
+        private void buttonDisconnect_Click(object sender, EventArgs e)
+        {
+            if (port != null && port.IsOpen)
+            {
+                timer.Stop();
+                port.Close();
+                port.Dispose();
+            }
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                if (port != null && port.IsOpen)
+                {
+                    byte slaveId = 2;
+                    ushort startAddress = 0;
+                    ushort numRegisters = 16; // 8 adet float veri için 16 adet register okunmalı
+
+                    ushort[] registers = master.ReadHoldingRegisters(slaveId, startAddress, numRegisters);
+
+                    for (int i = 0; i < 8; i++)
+                    {
+                        float value = ConvertRegistersToFloat(registers[i * 2 + 1], registers[i * 2]);
+                        pointPairLists[i].Add(time, value);
+                    }
+                    time++;
+
+                    zedGraphControl.AxisChange();
+                    zedGraphControl.Invalidate();
+                }
+                else
+                {
+                    timer.Stop();
+                    MessageBox.Show("Seri port açık değil!");
+                }
+            }
+            catch (Exception ex)
+            {
+                timer.Stop();
+                MessageBox.Show($"Hata: {ex.Message}");
+            }
+        }
+
+        private float ConvertRegistersToFloat(ushort highOrderByte, ushort lowOrderByte)
+        {
+            // 2 adet 8-bit byte'ı 16-bit değere dönüştür
+            ushort combinedValue = (ushort)((highOrderByte << 8) | lowOrderByte);
+
+            // Dönüştürülmüş 16-bit değeri float'a dönüştür
+            return combinedValue / 10.0f; // sıcaklık değeri 10'a bölünerek float değeri elde ediliyor
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            try
+            {
+                if (port != null && port.IsOpen)
+                {
+                    port.Close();
+                    port.Dispose();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Seri port kapatılırken hata oluştu: {ex.Message}");
+            }
+
+            base.OnFormClosing(e);
+        }
+    }
+}
